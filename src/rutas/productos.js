e36d652dbe0daca795f41a064f879641d7b13e8b
SiliconVilla <asocia2.co@gmail.com>
2021-09-30 22:21:21 -0500

    Completando Crud Productos

--- src/rutas/productos.js
+++ src/rutas/productos.js
@@ -1,10 +1,24 @@
 const { Router } = require('express');
 const router = Router();
+//Conectar a Base de Datos
 
 //Listar Productos y será la raíz
 router.get('/', (req, res) => {
     //res.send('Listado de productos, configurar la vista');
     res.render('productos/listadoProductos');
+});
+
+//Agregar Productos
+router.get('/agregarProductos', (req, res) => {
+    //res.send('Listado de productos, configurar la vista');
+    res.render('productos/agregarProductos');
+});
+
+
+//Editar Productos
+router.get('/editarProductos', (req, res) => {
+    //res.send('Listado de productos, configurar la vista');
+    res.render('productos/editarProductos');
 });
 
 //Exportar
